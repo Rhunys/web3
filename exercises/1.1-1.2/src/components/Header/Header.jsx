416515ab4d81e5b996b06c03e0d53d241a5da685
GauthierC <gauthier.collard@student.vinci.be>
2024-09-18 08:35:23 +0200

    1.1-1.2 : import image

--- exercises/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import logo from 'components/Header/LOGO HE VINCI.png'
+
 const Header = (props) => {
     return (
         <div>
+        <img src={logo}/>
         <h1>
             {props.course}
         </h1>
