0a6eb6b302a504f51ad3c1be2ae706a159eed3ce
GauthierC <gauthier.collard@student.vinci.be>
2025-01-02 16:08:39 +0100

    fix: graphql-backend

---
+++ tutorials/module8/graphql-frontend/src/components/Persons/Persons.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+
+const Persons = ({ persons }) => {
+    return (
+      <div>
+        <h2>Persons</h2>
+        {persons.map(p =>
+          <div key={p.name}>
+            {p.name} {p.phone}
+          </div>  
+        )}
+      </div>
+    )
+  }
+
+export { Persons }
